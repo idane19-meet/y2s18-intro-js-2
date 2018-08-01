115455c306f3355054041c6d6b80c9e67e40fd80
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:14:02 +0300

    asjkfhj

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,12 @@
 // Create your 'me' object literal here!
+var me = 
+{
+	firstName:"Idan",
+	lastName:"efraim",
+	favoriteFoods:["Pizza","Hamburger"],
+	age:16,
+	picture:"https://www.google.co.il/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiN8t6g0cvcAhUIalAKHdtED_cQjRx6BAgBEAU&url=https%3A%2F%2Fwww.si.com%2Fnba%2F2018%2F03%2F27%2Fstephen-curry-injury-warriors-rockets-nba-playoffs-kevin-durant&psig=AOvVaw0XKxkirNPWZ_YaMfE9D_m6&ust=1533205365928862"
+}
+
+var me1 = JSON.stringify(me);
+document.write(me1);
